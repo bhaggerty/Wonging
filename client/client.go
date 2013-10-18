8b45725295d80961a25db9784eddd4dccf654cb0
Joey Zhou <josephyzhou@gmail.com>
2013-10-18 12:23:38 -0700

    putting client in its own folder

--- client/client.go
+++ client/client.go
@@ -1,9 +1,11 @@
-package wonging
+package main
 
 import (
+	"fmt"
 	"github.com/josephyzhou/wonging"
 )
 
 func main() {
+	deck := new(wonging.Deck).initDeck()
 	fmt.Println("This is the client")
 }
