7a8c59dcc6a4358b5469ac1e84c3640580f6fa22
Joey Zhou <josephyzhou@gmail.com>
2013-10-20 17:51:16 -0700

    more unit tests for hand class

--- tests/hand_test.go
+++ tests/hand_test.go
@@ -2,10 +2,11 @@ package wonging
 
 import (
 	"github.com/josephyzhou/wonging"
+	"strconv"
 	"testing"
 )
 
-func Test_calculateValue(t *testing.T) {
+func predefineHand() *wonging.Hand {
 	card1 := new(wonging.Card).NewCard("9", int8(9), "Clubs")
 	card2 := new(wonging.Card).NewCard("J", int8(10), "Hearts")
 	card3 := new(wonging.Card).NewCard("K", int8(10), "Diamonds")
@@ -13,10 +14,25 @@ func Test_calculateValue(t *testing.T) {
 	hand.AddCard(card1)
 	hand.AddCard(card2)
 	hand.AddCard(card3)
+	return hand
+}
+
+func Test_CalculateValue(t *testing.T) {
+	hand := predefineHand()
 	totalValue := hand.CalculateValue()
 	if totalValue != 29 {
 		t.Error("CalculateValue() did not work as expected.")
 	} else {
 		t.Log("CalculateValue() test passed")
+	}
+}
+
+func Test_CalculateCount(t *testing.T) {
+	hand := predefineHand()
+	counter := hand.CalculateCount()
+	if counter.HiLo != -2 {
+		t.Error("CalculateCount() - Expected: -2, Actual: " + strconv.Itoa(int(counter.HiLo)))
+	} else {
+		t.Log("CalculateCount() HiLo test passed")
 	}
 }
