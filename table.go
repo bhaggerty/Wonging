4d0adab6ec3f2e230a231049c9e21509b47d735a
Joey Zhou <josephyzhou@gmail.com>
2013-10-22 02:24:02 -0700

    players dealers

--- table.go
+++ table.go
@@ -1,29 +1,31 @@
-package Wonging
+package wonging
 
 import (
-	"fmt"
-	"math/rand"
-	"strconv"
-	"time"
+// "fmt"
+// "math/rand"
+// "strconv"
+// "time"
 )
 
+type Player struct {
+	hand       *Hand
+	currentBet float32
+	totalCash  float32
+}
+type Dealer struct {
+	shoe *Deck
+}
 type Table struct {
 	//current count for all cards on table
 	count int8
 
-	//pot money in middle
-	pot float32
-
-	//total value of table
-	totalCash float32
-
 	//assuming one deck of card per dealer
-	shoe *Deck
+	dealer *Dealer
 
 	//Many players sitting at the table
-	hands []*Hand
+	players []*Player
 }
 
-func (t *Table) GetTablePlayerNumber() {
-	return len(t.hands)
+func (t *Table) GetTablePlayerNumber() int {
+	return len(t.players)
 }
