b3713cd05c9759be880cbfdde5f77d53ee8c940f
Joey Zhou <josephyzhou@gmail.com>
2013-10-23 17:19:23 -0700

    adding bet to player

--- player.go
+++ player.go
@@ -1,9 +1,22 @@
 package Wonging
 
 type Player struct {
-	id         uint8
-	tableId    int8
-	hand       *Hand
+	id uint8
+	//current table the player is sitting at
+	tableId int8
+	//current hand
+	hand *Hand
+	//how much is the player betting
 	currentBet float32
-	totalCash  float32
+	//how much money does the player have
+	totalCash float32
+}
+
+func (p *Player) bet(money float32) {
+	if money <= 0 {
+		fmt.Println("Invalid bet")
+	} else {
+		p.currentBet += money
+		p.totalCash -= money
+	}
 }
