8815b6e3cf734f2ce60c84c520edd6d280ef83b1
Joey Zhou <josephyzhou@gmail.com>
2013-10-23 18:32:46 -0700

    adding three more function to casino class

--- casino.go
+++ casino.go
@@ -1,9 +1,52 @@
 package wonging
 
+import (
+	"fmt"
+)
+
 type Casino struct {
+	bank        float64
 	tables      []*Table
 	idleDealers []*Dealer
 	idlePlayers []*Player
+}
+
+func (c *Casino) Initialize() *Casino {
+	c.bank = DEFAULTCASINOSTARTINGCASH
+	for i := 0; i < DEFAULTNUMBEROFTABLESPERCASINO; i++ {
+		c.tables = append(c.tables, new(Table).Initialize(uint8(i)))
+	}
+	c.idleDealers = nil
+	c.idlePlayers = nil
+	return c
+}
+
+func (c *Casino) dealerBecomesIdle(d *Dealer) {
+	dealerAlreadyIdle := false
+	for _, iD := range c.idleDealers {
+		if iD == d {
+			dealerAlreadyIdle = true
+		}
+	}
+	if dealerAlreadyIdle {
+		fmt.Println("Dealer already idle")
+	} else {
+		c.idleDealers = append(c.idleDealers, d)
+	}
+}
+
+func (c *Casino) playerBecomesIdle(d *Player) {
+	playerAlreadyIdle := false
+	for _, iP := range c.idlePlayers {
+		if iP == d {
+			playerAlreadyIdle = true
+		}
+	}
+	if playerAlreadyIdle {
+		fmt.Println("Player already idle")
+	} else {
+		c.idlePlayers = append(c.idlePlayers, d)
+	}
 }
 
 //calculate total cash flow
--- simulator.go
+++ simulator.go
@@ -8,3 +8,9 @@ const DEFAULTPLAYERLIMITPERTABLE uint8 = 5
 
 // how many chances a player gets caught before he's banned
 const STRIKEPERPLAYER uint8 = 3
+
+//how much money casino opens with - 1 billion
+const DEFAULTCASINOSTARTINGCASH float64 = 1000000000
+
+//how many blackjack tables are there in this casino
+const DEFAULTNUMBEROFTABLESPERCASINO int = 10
