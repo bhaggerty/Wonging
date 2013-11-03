40e176c8461b02cdfeb5fe9cf66fcbb4b248d029
Joey Zhou <josephyzhou@gmail.com>
2013-11-02 22:30:12 -0700

    simulator.go: init a casino and print

--- client/simulator.go
+++ client/simulator.go
@@ -6,10 +6,12 @@ import (
 
 func main() {
 
-	deck := new(wonging.Deck).Initialize(1)
-	deck.PrintDeck()
+	// deck := new(wonging.Deck).Initialize(1)
+	// deck.PrintDeck()
 	// deck = deck.pop()
 	// deck = deck.DealRandom()
 	// deck = deck.DealRandom()
 	// deck = deck.Shuffle()
+	casino := new(wonging.Casino).Initialize(0)
+	casino.PrintCasino()
 }
