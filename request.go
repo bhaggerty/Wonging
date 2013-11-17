27a7964ac01a7e367715c8049e400cbdf0723f5d
Joey Zhou <josephyzhou@gmail.com>
2013-11-17 12:56:36 -0800

    generic print request for logging

--- request.go
+++ request.go
@@ -1,8 +1,16 @@
 package wonging
 
+import (
+	"fmt"
+)
+
 type Request struct {
 	entityType string //possible value "player", "dealer"
 	id         uint8  //id of entity
 	action     string //possible value "hit", "stand", "double"
 	handIndex  uint8  //which hand should we apply the action, 0+
+}
+
+func (r *Request) printRequest() {
+	fmt.Printf("%s %d requests %s [%d]\n", r.entityType, r.id, r.action, r.handIndex)
 }
