710bc634daa932d610a96ec8ed8fb5b5eeff7087
Joey Zhou <josephyzhou@gmail.com>
2013-11-17 10:35:20 -0800

    random selection of strategy, return action string instead of executing function

--- dealerStrategies.go
+++ dealerStrategies.go
@@ -1,18 +1,30 @@
+//similar to player strategies class
+//To use: first init a Dealer's strategy in init function
+//call simulate(), which would in turn call one of these strategy functions
 package wonging
 
-type DealerStrategies interface {
-	standsOnAll17(d *Dealer) *Dealer
-	hitOnSoft17(d *Dealer) *Dealer
+// type DealerStrategies interface {
+// 	standsOnAll17(d *Dealer) *Dealer
+// 	hitOnSoft17(d *Dealer) *Dealer
+// }
+//
+type DealerStrategy func(*Dealer) string
+
+func randomDealerStrategy() DealerStrategy {
+	strategies := []DealerStrategy{standsOnAll17, hitOnSoft17}
+	return strategies[randInt(0, len(strategies)-1)]
 }
 
 //Dealer strategies
-func standsOnAll17(d *Dealer) {
+func standsOnAll17(d *Dealer) string {
 	if value, _ := d.calculateHandValue(); value < 17 {
-		d.dealSelf()
+		return "dealSelf"
 	}
+	return ""
 }
-func hitOnSoft17(d *Dealer) {
+func hitOnSoft17(d *Dealer) string {
 	if value, soft := d.calculateHandValue(); (value >= 17 && soft) || value < 17 {
-		d.dealSelf()
+		return "dealSelf"
 	}
+	return ""
 }
