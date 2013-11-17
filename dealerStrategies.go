dd68039eaf2caea4d1e47c34a9d30a28f127c941
Joey Zhou <josephyzhou@gmail.com>
2013-11-17 12:56:11 -0800

    bug fix, logging the strategies used

--- dealerStrategies.go
+++ dealerStrategies.go
@@ -3,6 +3,10 @@
 //call simulate(), which would in turn call one of these strategy functions
 package wonging
 
+import (
+	"fmt"
+)
+
 // type DealerStrategies interface {
 // 	standsOnAll17(d *Dealer) *Dealer
 // 	hitOnSoft17(d *Dealer) *Dealer
@@ -12,19 +16,22 @@ type DealerStrategy func(*Dealer) string
 
 func randomDealerStrategy() DealerStrategy {
 	strategies := []DealerStrategy{standsOnAll17, hitOnSoft17}
-	return strategies[randInt(0, len(strategies)-1)]
+	return strategies[randInt(0, len(strategies))]
 }
 
 //Dealer strategies
 func standsOnAll17(d *Dealer) string {
+	fmt.Print("[strategy: standsOnAll17]: ")
 	if value, _ := d.calculateHandValue(); value < 17 {
 		return "dealSelf"
 	}
-	return ""
+	return "stand"
 }
 func hitOnSoft17(d *Dealer) string {
+	fmt.Print("[strategy: hitOnSoft17]: ")
+
 	if value, soft := d.calculateHandValue(); (value >= 17 && soft) || value < 17 {
 		return "dealSelf"
 	}
-	return ""
+	return "stand"
 }
--- playerStrategies.go
+++ playerStrategies.go
@@ -3,6 +3,10 @@
 //call simulate(), which would in turn call one of these strategy functions
 package wonging
 
+import (
+	"fmt"
+)
+
 // type PlayerStrategies interface {
 // 	basic(p *Player) *Player
 // }
@@ -10,11 +14,13 @@ type PlayerStrategy func(*Player) (string, uint8)
 
 func randomPlayerStrategy() PlayerStrategy {
 	strategies := []PlayerStrategy{basic}
-	return strategies[randInt(0, len(strategies)-1)]
+	return strategies[randInt(0, len(strategies))]
 }
 
 //a basic strategy
 func basic(p *Player) (string, uint8) {
+	fmt.Print("[strategy: basic]: ")
+
 	for i := 0; i < len(p.hands); i++ {
 		if value, _ := p.calculateHandValue(uint8(i)); value < 17 {
 			return "hit", uint8(i)
@@ -22,7 +28,7 @@ func basic(p *Player) (string, uint8) {
 			return "stand", uint8(i)
 		}
 	}
-	return "", 0
+	return "stand", 0
 }
 
 func doubleDown() {
