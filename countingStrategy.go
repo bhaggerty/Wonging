3d58355edd491564275e208969efcb1d35036b96
Joey Zhou <josephyzhou@gmail.com>
2013-12-10 22:41:28 -0800

    counting strategy assignment shit

--- countingStrategy.go
+++ countingStrategy.go
@@ -134,3 +134,42 @@ func (c *Counter) count(cardValue string) *Counter {
 	c.ZenCount += ZenCount[cardValue]
 	return c
 }
+
+// strategies
+func getHiLo(c *Counter) float32 {
+	return float32(c.HiLo)
+}
+
+func getHiOpt1(c *Counter) float32 {
+	return float32(c.HiOpt1)
+}
+
+func getHiOpt2(c *Counter) float32 {
+	return float32(c.HiOpt2)
+}
+
+func getKO(c *Counter) float32 {
+	return float32(c.KO)
+}
+
+func getOmega2(c *Counter) float32 {
+	return float32(c.Omega2)
+}
+
+func getRed7(c *Counter) float32 {
+	return float32(c.Red7)
+}
+
+func getZenCount(c *Counter) float32 {
+	return float32(c.ZenCount)
+}
+
+// strategies assignments
+type CountingStrategy func(*Counter) float32
+
+func randomCountingStrategy() (CountingStrategy, string) {
+	strategies := []CountingStrategy{getHiLo, getHiOpt1, getHiOpt2, getKO, getOmega2, getRed7, getZenCount}
+	description := []string{"Hi Lo", "Hi Opt 1", "Hi Opt 2", "KO", "Omega 2", "Red 7", "Zen Count"}
+	randomInt := randInt(0, len(strategies))
+	return strategies[randomInt], description[randomInt]
+}
