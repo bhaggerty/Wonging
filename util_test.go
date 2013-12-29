d960d67ab6e433afd025794f1288a2093ee208c5
Joey Zhou <josephyzhou@gmail.com>
2013-12-29 17:27:52 -0500

    min max float tests

--- util_test.go
+++ util_test.go
@@ -13,3 +13,19 @@ func Test_CheckCardsValueEqual(t *testing.T) {
 		t.Error("CheckCardsValueEqual() did not work as expected.")
 	}
 }
+
+func Test_MinMaxFloatS(t *testing.T) {
+	v := []float64{1.2, 2.4, 3.6}
+	if MinFloatS(v) != 0 {
+		t.Error("MinFloatS() did not work as expected.")
+	} else {
+		t.Log("MinFloatS() test passed")
+	}
+
+	if MaxFloatS(v) != 2 {
+		t.Error("MaxFloatS() did not work as expected.")
+	} else {
+		t.Log("MaxFloatS() test passed")
+	}
+
+}
