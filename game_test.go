3e54cf16742493ac66bb0a69981d216f34b7532b
Joey Zhou <josephyzhou@gmail.com>
2014-01-06 19:46:22 -0800

    test game predefined, biggest winner/loser test

--- game_test.go
+++ game_test.go
@@ -4,20 +4,36 @@ import (
 	"testing"
 )
 
+func predefineGame() *Game {
+	table := predefineTable()
+	return new(Game).Initialize(table)
+}
+
+func predefineGameWithPlayerResults() *Game {
+	table := predefineTableWithPlayers()
+	if table != nil {
+		return new(Game).Initialize(table)
+	}
+	return nil
+}
+
 func Test_InitializeGame(t *testing.T) {
-	table := new(Table).Initialize(1, new(Casino).Initialize(0))
-	game := new(Game).Initialize(table)
+	game := predefineGame()
 	if game.table == nil {
 		t.Error("Initialize() [table attribute] did not work as expected.")
 	} else if game.casinoEarning != 0 {
 		t.Error("Initialize() [casinoEarning attribute] did not work as expected.")
-	} else if !(len(table.players) == 0 && game.playerResult == nil) {
-		t.Error("Initialize() [playerResult attribute] did not work as expected.")
 	} else {
 		t.Log("Initialize() test passed")
 	}
 }
 
-func Test_updatePlayerResult(t *testing.T) {
-
+func Test_biggestWinnerLoser(t *testing.T) {
+	game := predefineGameWithPlayerResults()
+	if game.biggestWinner() == 0 {
+		t.Log("biggestWinner() test passed")
+	}
+	if game.biggestLoser() == 1 {
+		t.Log("biggestLoser() test passed")
+	}
 }
