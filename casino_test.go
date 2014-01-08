f5b0e616b1138940fdb287ed5916c216f22b4f05
Joey Zhou <josephyzhou@gmail.com>
2014-01-07 20:55:24 -0800

    casino init test

--- casino_test.go
+++ casino_test.go
@@ -1,9 +1,26 @@
 package wonging
 
 import (
-// "testing"
+	"testing"
 )
 
 func predefineCasino() *Casino {
 	return new(Casino).Initialize(1)
+}
+
+func Test_InitializeCasino(t *testing.T) {
+	casino := predefineCasino()
+	if casino.id != 1 {
+		t.Error("Initialize() [id attribute] did not work as expected.")
+	} else if casino.bank != DEFAULTCASINOSTARTINGCASH {
+		t.Error("Initialize() [bank attribute] did not work as expected.")
+	} else if casino.tables == nil || len(casino.tables) != DEFAULTNUMBEROFTABLESPERCASINO {
+		t.Error("Initialize() [tables attribute] did not work as expected.")
+	} else if casino.idleDealers == nil || len(casino.idleDealers) != DEFAULTNUMBEROFDEALERSPERCASINO {
+		t.Error("Initialize() [idleDealers attribute] did not work as expected.")
+	} else if casino.idlePlayers == nil || len(casino.idlePlayers) != DEFAULTNUMBEROFPLAYERSPERCASINO {
+		t.Error("Initialize() [idlePlayers attribute] did not work as expected.")
+	} else {
+		t.Log("Initialize() test passed")
+	}
 }
